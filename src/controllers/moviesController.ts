bddad1f6c8211cce70963cbc705da437c644a805
Nikolas Menezes <nikolasdev.contato@gmail.com>
2023-10-29 16:03:04 -0300

    Finalizando o CRUD da API

--- src/controllers/moviesController.ts
+++ src/controllers/moviesController.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 
 import { MovieModel } from '../models/movie';
 
@@ -10,34 +10,94 @@ export async function createMovie(req: Request, res: Response) {
 
     return res.status(201).json(movie)
   }
-  catch (e: any){
+  catch (e: any) {
     console.error(e)
+    return res.status(500).json({ erro: e })
   }
 
 }
 
-export async function getMovieById(req: Request, res: Response){
+export async function getMovieById(req: Request, res: Response) {
 
   try {
-    
+
     const id = req['params']['id']
 
     const movie = await MovieModel.findById(id);
 
-    if(!movie){
-      return res.status(404).json({erro: "Filme não encontrado!"})
+    if (!movie) {
+      return res.status(404).json({ erro: "Filme não encontrado!" })
     }
 
     return res.status(200).json(movie)
 
-  } 
-  catch (e: any){
+  }
+  catch (e: any) {
     console.error(e)
+    return res.status(500).json({ erro: e })
   }
 
 }
 
-export default {
-  createMovie,
-  getMovieById
+export async function getMovies(req: Request, res: Response) {
+
+  try {
+    const movies = await MovieModel.find({}).all('stars', []);
+    if (!movies) {
+      return res.status(404).json({ 'erro': 'Nenhum filme encontrado!' })
+    }
+
+    return res.status(200).json(movies)
+  }
+  catch (e: any) {
+    console.error(e)
+    return res.status(500).json({ erro: e })
+  }
+}
+
+export async function updateMovies(req: Request, res: Response) {
+
+  try {
+    const id = req['params']['id']
+    const data = req['body']
+
+    const movie = MovieModel.findById(id)
+
+    if (!movie) {
+      return res.status(404).json({ erro: "o Filme não existe!" })
+    }
+
+    await MovieModel.updateOne({ _id: id }, data)
+
+    return res.status(200).json({ msg: "Filme atualizado com sucesso!" })
+
+  }
+  catch (e) {
+    console.error(e)
+    return res.status(500).json({ erro: e })
+  }
+
+}
+
+export async function deleteMovies(req: Request, res: Response) {
+
+  try {
+    const id = req['params']['id']
+
+    const movie = MovieModel.findById(id)
+
+    if (!movie) {
+      return res.status(404).json({ erro: "o Filme não existe!" })
+    }
+
+    MovieModel.deleteOne(movie)
+
+    return res.status(200).json({ msg: "Filme deletado com sucesso!" })
+
+  }
+  catch (e) {
+    console.error(e)
+    return res.status(500).json({ erro: e })
+  }
+
 }
--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,7 @@ const router = Router()
 import { movieValidator } from './middlewares/movieValidator';
 import { validate } from './middlewares/handleValidation';
 
-import { createMovie, getMovieById } from './controllers/moviesController'
+import { createMovie, deleteMovies, getMovieById, getMovies, updateMovies } from './controllers/moviesController'
 
 router.get('/test', (req: Request, res: Response) => {
   return res.status(200).send('API Working!')
@@ -13,5 +13,8 @@ router.get('/test', (req: Request, res: Response) => {
 
 router.post('/movie/create', movieValidator(), validate, createMovie);
 router.get('/movie/:id', getMovieById);
+router.get('/movie/all', getMovies);
+router.patch('/movie/:id', movieValidator(), validate, updateMovies);
+router.delete('/movie/:id', deleteMovies);
 
 export default router
